69ca5fef638f402af30bf1bb13d1a881dbf337f0
oxycoon <daniel.dimby@gmail.com>
2016-09-20 10:52:55 +0200

    Internal storage
    Finished internal storage of data, but still needs to be hooked up with
    system and tested.

--- BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/Data.java
+++ BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/Data.java
@@ -1,6 +1,9 @@
 package no.oxycoon.thesis.sensor;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by Elisabeth on 01.09.2016.
@@ -19,5 +22,28 @@ public class Data
     public Float getData(int index)
     {
         return _data.get(index);
+    }
+
+    public long getTimestamp()
+    {
+        return _timestamp;
+    }
+
+    public String toString()
+    {
+        /*Date date = new Date(_timestamp);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+
+        String result = sdf.format(date);*/
+        String result = _timestamp + "";
+
+        for(Float data : _data)
+        {
+            result += "," + _data ;
+        }
+        //result += "\n";
+
+        return result;
     }
 }
--- BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/FileManager.java
+++ BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/FileManager.java
@@ -1,5 +1,7 @@
 package no.oxycoon.thesis.sensor;
 
+import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
@@ -8,13 +10,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Created by Oxycoon on 17.09.2016.
+ * Created by Elisabeth on 17.09.2016.
  */
 public class FileManager
 {
-    public FileManager()
-    {
+    private final String LOG_TAG = "SensorTagFileManager";
 
+    private Context _context;
+
+    public FileManager(Context context)
+    {
+        _context = context;
     }
 
     public boolean writeFile(String filename, ArrayList<Data> data)
@@ -31,8 +37,10 @@ public class FileManager
 
     private boolean writeInternalFile(String name, ArrayList<Data> data)
     {
-        File file;
+        //File file = new File(_context.getFilesDir(), name);
         FileOutputStream outputStream;
+
+        name += ".csv";
 
         String string = "";
 
@@ -43,13 +51,80 @@ public class FileManager
 
         try
         {
-            outputStream = openFileOutput(name, Context.MODE_PRIVATE);
+            outputStream = _context.openFileOutput(name, Context.MODE_PRIVATE);
             outputStream.write(string.getBytes());
             outputStream.close();
+            return true;
         }
         catch (IOException e)
         {
             Log.d("SensorTagFileMangaer", "File writing failed: " + e.getMessage());
+            return false;
         }
+    }
+
+    private boolean writeExternalFile(String name, ArrayList<Data> data)
+    {
+        if(name.equals(""))
+        {
+            name = "SensorTag_" + data.get(0).getTimestamp();
+        }
+        name += ".csv";
+
+        if(isExternalStorageWritable())
+        {
+            if(!externalDirectoryExists())
+            {
+                return false;
+            }
+            File output = new File(_context.getExternalFilesDir(
+                    Environment.DIRECTORY_DOCUMENTS + "/SensorTag"), name);
+
+            FileOutputStream outputStream = new FileOutputStream(output);
+
+
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private boolean externalDirectoryExists()
+    {
+        File dir = new File(_context.getExternalFilesDir(
+                Environment.DIRECTORY_DOCUMENTS), "SensorTag");
+        if(!dir.exists())
+        {
+            if(!dir.mkdirs())
+            {
+                Log.e(LOG_TAG, "Directory not created");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isExternalStorageWritable()
+    {
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isExternalStorageReadable()
+    {
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state) ||
+           Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
+        {
+            return true;
+        }
+        return false;
     }
 }
