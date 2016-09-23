8ec101bb8062f385d9e391e0788c66ce44ef5daf
oxycoon <daniel.dimby@gmail.com>
2016-09-23 14:09:08 +0200

    Revert "DataType support and gui work"
    This reverts commit 04b03e7e85a6443ee44f968b069362db6d6b4b30.

--- BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/Data.java
+++ BleSensorTag/src/main/java/no/oxycoon/thesis/sensor/Data.java
@@ -5,61 +5,21 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.TimeZone;
 
-enum DataType
-{
-    ACCELEROMETER_X,
-    ACCELEROMETER_Y,
-    ACCELEROMETER_Z,
-    GYRO_X,
-    GYRO_Y,
-    GYRO_Z,
-    BAROMETER,
-    UNKNOWN
-}
-
 /**
  * Created by Elisabeth on 01.09.2016.
- *
- * Denotes a data node collected at a time by a sensor. Data can be from barometer,
- * acceleromter and/or gyro.
  */
+
 public class Data
 {
-    /**
-     *  Data values
-     */
     private ArrayList<Float> _data;
-
-    /**
-     *  Data type at index in _data
-     */
-    private ArrayList<DataType> _dataType;
-
-    /**
-     *  Timestamp at which the data is recorded
-     */
     private long _timestamp;
 
-    /**
-     * Constructor where only timestamp is known.
-     *
-     * @param timestamp
-     *  Time at which the data is recorded
-     */
     public Data(int timestamp)
     {
         _timestamp = timestamp;
         _data = new ArrayList<Float>();
     }
 
-    /**
-     * Constructor where the amount of data points is known.
-     *
-     * @param timestamp
-     *  Time at which the data is recorded
-     * @param size
-     *  How many data points is in the record
-     */
     public Data(int timestamp, int size)
     {
         _timestamp = timestamp;
@@ -68,18 +28,9 @@ public class Data
         for(int i = 0; i < size; i++)
         {
             _data.add(0.0f);
-            _dataType.add(DataType.UNKNOWN);
         }
     }
 
-    /**
-     * Gets data at index.
-     *
-     * @param index
-     *  Index for the data
-     * @return
-     *  Value of data at index
-     */
     public Float getData(int index)
     {
         return _data.get(index);
@@ -90,16 +41,14 @@ public class Data
         return _timestamp;
     }
 
-    public void addData(float data, DataType type)
+    public void addData(float data)
     {
         _data.add(data);
-        _dataType.add(type);
     }
 
-    public void modifyData(int index, float data, DataType type)
+    public void modifyData(int index, float data)
     {
         _data.set(index, data);
-        _dataType.set(index, type);
     }
 
     public String toString()
